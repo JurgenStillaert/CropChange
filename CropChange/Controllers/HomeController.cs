ec8637b8a751bfe90e40a9890888e8a1d966968e
Jurgen Stillaert <jurgen@stillaert.be>
2023-10-05 14:08:32 +0200

    Add project files.

---
+++ CropChange/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿// Controllers/HomeController.cs
+using Crops.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Crops.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Using a static list as a cache for demo purposes
+        private static List<Crops.Models.CropChange> _changes = new List<Crops.Models.CropChange>();
+
+        public IActionResult Index()
+        {
+            return View(_changes);
+        }
+
+        [HttpPost]
+        public IActionResult Submit(Crops.Models.CropChange change)
+        {
+            change.ModificationDate = DateTime.Now;
+            _changes.Add(change);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CropChange/Models/CropChange.cs
@@ -0,0 +1,12 @@
+﻿// Models/CropChange.cs
+namespace Crops.Models
+{
+    public class CropChange
+    {
+        public DateTime ModificationDate { get; set; }
+        public int ParcelId { get; set; }
+        public string NewEarlyCrop { get; set; } = "Green cover";
+        public string NewMainCrop { get; set; }
+        public string NewLateCrop { get; set; } = "Green cover";
+    }
+}
